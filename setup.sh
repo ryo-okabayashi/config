a34583de2c549a4696c8e246d79286b1b23816e0
Ryo Okabayashi <okabayashi@greenish.jp>
2017-01-10 17:15:49 +0900

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+## vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+cd ~/.vim/bundle
+git clone git://github.com/altercation/vim-colors-solarized.git
+
+mv ~/.vimrc ~/.vimrc.backup
+curl -LSso ~/.vimrc https://raw.githubusercontent.com/ryo-okabayashi/config/master/.vimrc
+
+## tmux
+curl -LSso ~/.tmux.conf https://raw.githubusercontent.com/ryo-okabayashi/config/master/.tmux.conf
