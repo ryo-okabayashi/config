df2342b6d926d4196f808f3a98fe009461475ec8
Ryo Okabayashi <ryo.okabayashi@gmail.com>
2017-02-23 18:36:07 +0900

    powerline

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 cd ~/.vim/bundle
 git clone git://github.com/altercation/vim-colors-solarized.git
+git cone https://github.com/Lokaltog/vim-powerline.git
 
 mv ~/.vimrc ~/.vimrc.backup
 curl -LSso ~/.vimrc https://raw.githubusercontent.com/ryo-okabayashi/config/master/.vimrc
