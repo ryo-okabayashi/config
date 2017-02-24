72b1e8930d3be5bed0d708b45daa7ffc63928cb7
Ryo Okabayashi <ryo.okabayashi@gmail.com>
2017-02-24 09:29:00 +0900

    airline

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 cd ~/.vim/bundle
 git clone git://github.com/altercation/vim-colors-solarized.git
-git clone https://github.com/Lokaltog/vim-powerline.git
+git clone https://github.com/vim-airline/vim-airline
 
 mv ~/.vimrc ~/.vimrc.backup
 curl -LSso ~/.vimrc https://raw.githubusercontent.com/ryo-okabayashi/config/master/.vimrc
