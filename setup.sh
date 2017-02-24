f3dae400d5656fde023fea980e3b2968019f5a7a
Ryo Okabayashi <ryo.okabayashi@gmail.com>
2017-02-24 11:01:20 +0900

    vim git

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
 git clone git://github.com/altercation/vim-colors-solarized.git
 git clone https://github.com/vim-airline/vim-airline
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/airblade/vim-gitgutter.git
 
 mv ~/.vimrc ~/.vimrc.backup
 curl -LSso ~/.vimrc https://raw.githubusercontent.com/ryo-okabayashi/config/master/.vimrc
