8db2161771c9d698eacea2ce2c3df16b1f3e5b0e
Ryo Okabayashi <ryo.okabayashi@gmail.com>
2017-06-16 12:39:58 +0900

    Create vimupdate.sh

---
+++ vimupdate.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd ~/.vim/bundle/nerdtree
+git pull
+cd ~/.vim/bundle/vim-airline
+git pull
+cd ~/.vim/bundle/vim-colors-solarized
+git pull
+cd ~/.vim/bundle/vim-fugitive
+git pull
+cd ~/.vim/bundle/vim-gitgutter
